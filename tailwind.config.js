0b36a49e1b3c89fc8a9f40e9f2628accc63eb3e6
azzamdinabdillah <azzamdinabdillah123@gmail.com>
2023-02-22 08:02:00 +0700

    Update Navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,27 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'patua': ['Patua One', 'cursive'],
+        patua: ["Patua One", "cursive"],
       },
       colors: {
-        'primary': '#FE5E44',
-        'dark-text': '#0E144A'
+        primary: "#FE5E44",
+        "dark-text": "#0E144A",
+      },
+      keyframes: {
+        openNav: {
+          "0%": { height: "12vh" },
+          "100%": { height: "60vh" },
+        },
+        closeNav: {
+          "0%": { height: "60vh" },
+          "100%": { height: "12vh" },
+        },
+      },
+      animation: {
+        "anim-navbar": "openNav 0.3s ease-in-out",
+        "anim-navbar-close": "closeNav 0.3s ease-in-out",
       },
     },
   },
   plugins: [],
-}
+};
